bf736baa02f31885fe95ee04fe5287fcee16f703
Laercio <laerciorjunior@gmail.com>
2023-05-17 12:43:39 -0300

    create terrain pt 3

--- lib/src/screen/terrain_crud/creating_terrain.dart
+++ lib/src/screen/terrain_crud/creating_terrain.dart
@@ -11,7 +11,21 @@ class CreatingTerrain extends StatefulWidget {
   final Function changeAction;
   CreatingTerrain({super.key, required this.changeAction});
   final _pageController = PageController(initialPage: 0);
-  var pagesNumber = 1;
+
+  nextStep(int newPageNumber) {
+    forms.add(pageList[newPageNumber - 1]);
+    _pageController.animateToPage(newPageNumber, duration: const Duration(milliseconds: 750), curve: Curves.ease);
+  }
+
+  late List<Widget> forms = [
+    BasicInfoForm(onSubmit: () { nextStep(1);})
+  ];
+
+  late final pageList = [
+    SpecificInfoForm(onSubmit: () { nextStep(2); }),
+    PhotoForm(onSubmit: () { nextStep(3); }),
+    const PaymentForm()
+  ];
 
   @override
   State<CreatingTerrain> createState() => _CreatingTerrainState();
@@ -24,7 +38,7 @@ class _CreatingTerrainState extends State<CreatingTerrain> {
   }*/
 
   // Organizar a zona repetitiva logo abaixo assim que houver tempo
-  late var photoForm = PhotoForm(onSubmit: () {
+  /*late var photoForm = PhotoForm(onSubmit: () {
     if (forms.length == 3) {
       forms.add(const PaymentForm());
     }
@@ -37,16 +51,7 @@ class _CreatingTerrainState extends State<CreatingTerrain> {
       forms.add(photoForm);
     }
     widget._pageController.animateToPage(2, duration: const Duration(milliseconds: 750), curve: Curves.ease);
-  });
-
-  late List<Widget> forms = [
-    BasicInfoForm(onSubmit: () {
-      if (forms.length == 1) {
-        forms.add(specificForm);
-      }
-      widget._pageController.animateToPage(1, duration: const Duration(milliseconds: 750), curve: Curves.ease);
-    },)
-  ];
+  });*/
 
   @override
   Widget build(BuildContext context) {
@@ -64,6 +69,7 @@ class _CreatingTerrainState extends State<CreatingTerrain> {
                 LabeledIconButton(
                   label: 'Info Gerais',
                   icon: const Icon(Icons.edit_square),
+                  color: Colors.black,
                   pressedFunction: () => {
                     widget._pageController.animateToPage(0, duration: const Duration(milliseconds: 750), curve: Curves.ease)
                   },
@@ -83,6 +89,7 @@ class _CreatingTerrainState extends State<CreatingTerrain> {
                 LabeledIconButton(
                   label: 'Detalhes',
                   icon: const Icon(Icons.map),
+                  color: Colors.black,
                   pressedFunction: () => {
                     widget._pageController.animateToPage(1, duration: const Duration(milliseconds: 750), curve: Curves.ease)
                   },
@@ -101,7 +108,8 @@ class _CreatingTerrainState extends State<CreatingTerrain> {
                 ),
                 LabeledIconButton(
                   label: 'Ad. Fotos',
-                  icon: const Icon(Icons.add_photo_alternate_outlined),
+                  icon: const Icon(Icons.add_photo_alternate),
+                  color: Colors.black,
                   pressedFunction: () => {
                     widget._pageController.animateToPage(2, duration: const Duration(milliseconds: 750), curve: Curves.ease)
                   },
@@ -120,9 +128,10 @@ class _CreatingTerrainState extends State<CreatingTerrain> {
                 ),
                 LabeledIconButton(
                   label: 'Pagamento',
-                  icon: const Icon(Icons.receipt_long_outlined),
+                  icon: const Icon(Icons.receipt_long),
+                  color: Colors.black,
                   pressedFunction: () => {
-                    widget._pageController.animateToPage(2, duration: const Duration(milliseconds: 750), curve: Curves.ease)
+                    
                   },
                 )
               ],
@@ -136,7 +145,7 @@ class _CreatingTerrainState extends State<CreatingTerrain> {
                 pageSnapping: true,
                 controller: widget._pageController,
                 itemBuilder: (context, pagePostion) {
-                  return forms[pagePostion];
+                  return widget.forms[pagePostion];
                 }
               )
             ),
