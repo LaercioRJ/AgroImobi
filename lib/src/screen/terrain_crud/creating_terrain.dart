941e858be263765f007631a3023165a0f0dfaf04
Laercio <laerciorjunior@gmail.com>
2023-05-17 13:50:49 -0300

    create terrain pt 4

--- lib/src/screen/terrain_crud/creating_terrain.dart
+++ lib/src/screen/terrain_crud/creating_terrain.dart
@@ -32,10 +32,10 @@ class CreatingTerrain extends StatefulWidget {
 }
 
 class _CreatingTerrainState extends State<CreatingTerrain> {
-  /*Future<bool> _onBackPressed() {
+  Future<bool> _onBackPressed() {
     widget.changeAction("Visualizar");
     return Future.value(true);
-  }*/
+  }
 
   // Organizar a zona repetitiva logo abaixo assim que houver tempo
   /*late var photoForm = PhotoForm(onSubmit: () {
@@ -55,102 +55,113 @@ class _CreatingTerrainState extends State<CreatingTerrain> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Criar Anúncio'),
-        backgroundColor: Colors.green,
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(2, 4, 2, 10),
-            child: Row(
-              children: [
-                LabeledIconButton(
-                  label: 'Info Gerais',
-                  icon: const Icon(Icons.edit_square),
-                  color: Colors.black,
-                  pressedFunction: () => {
-                    widget._pageController.animateToPage(0, duration: const Duration(milliseconds: 750), curve: Curves.ease)
-                  },
-                ),
-                Expanded(
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      border: Border(
-                        bottom: BorderSide(
-                          color: Colors.black,
-                          width: 2.0
+    List<Color> menuColors = [
+      Colors.green,
+      Colors.grey.shade400,
+      Colors.grey.shade400,
+      Colors.grey.shade400,
+    ];
+
+    return WillPopScope(
+      onWillPop: _onBackPressed,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Criar Anúncio'),
+          backgroundColor: Colors.green,
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(2, 4, 2, 10),
+              child: Row(
+                children: [
+                  LabeledIconButton(
+                    label: 'Info Gerais',
+                    icon: const Icon(Icons.edit_square),
+                    color: menuColors[0],
+                    pressedFunction: () => {
+                      widget._pageController.animateToPage(0, duration: const Duration(milliseconds: 750), curve: Curves.ease)
+                    },
+                  ),
+                  Expanded(
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(
+                            color: Colors.black,
+                            width: 2.0
+                          )
                         )
-                      )
-                    ),
-                  )
-                ),
-                LabeledIconButton(
-                  label: 'Detalhes',
-                  icon: const Icon(Icons.map),
-                  color: Colors.black,
-                  pressedFunction: () => {
-                    widget._pageController.animateToPage(1, duration: const Duration(milliseconds: 750), curve: Curves.ease)
-                  },
-                ),
-                Expanded(
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      border: Border(
-                        bottom: BorderSide(
-                          color: Colors.black,
-                          width: 2.0
+                      ),
+                    )
+                  ),
+                  LabeledIconButton(
+                    label: 'Detalhes',
+                    icon: const Icon(Icons.map),
+                    color: menuColors[1],
+                    pressedFunction: () => {
+                      widget._pageController.animateToPage(1, duration: const Duration(milliseconds: 750), curve: Curves.ease),
+                      menuColors[2] = Colors.green
+                    },
+                  ),
+                  Expanded(
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(
+                            color: Colors.black,
+                            width: 2.0
+                          )
                         )
-                      )
-                    ),
-                  )
-                ),
-                LabeledIconButton(
-                  label: 'Ad. Fotos',
-                  icon: const Icon(Icons.add_photo_alternate),
-                  color: Colors.black,
-                  pressedFunction: () => {
-                    widget._pageController.animateToPage(2, duration: const Duration(milliseconds: 750), curve: Curves.ease)
-                  },
-                ),
-                Expanded(
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      border: Border(
-                        bottom: BorderSide(
-                          color: Colors.black,
-                          width: 2.0
+                      ),
+                    )
+                  ),
+                  LabeledIconButton(
+                    label: 'Ad. Fotos',
+                    icon: const Icon(Icons.add_photo_alternate),
+                    color: menuColors[2],
+                    pressedFunction: () => {
+                      widget._pageController.animateToPage(2, duration: const Duration(milliseconds: 750), curve: Curves.ease)
+                    },
+                  ),
+                  Expanded(
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(
+                            color: Colors.black,
+                            width: 2.0
+                          )
                         )
-                      )
-                    ),
+                      ),
+                    )
+                  ),
+                  LabeledIconButton(
+                    label: 'Pagamento',
+                    icon: const Icon(Icons.receipt_long),
+                    color: menuColors[3],
+                    pressedFunction: () => {
+                      
+                    },
                   )
-                ),
-                LabeledIconButton(
-                  label: 'Pagamento',
-                  icon: const Icon(Icons.receipt_long),
-                  color: Colors.black,
-                  pressedFunction: () => {
-                    
-                  },
-                )
-              ],
-            )
-          ),
-          Expanded(
-            child: SizedBox(
-              width: MediaQuery.of(context).size.width,
-              child: PageView.builder(
-                itemCount: 4,
-                pageSnapping: true,
-                controller: widget._pageController,
-                itemBuilder: (context, pagePostion) {
-                  return widget.forms[pagePostion];
-                }
+                ],
               )
             ),
-          )
-        ],
+            Expanded(
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: PageView.builder(
+                  itemCount: 4,
+                  pageSnapping: true,
+                  controller: widget._pageController,
+                  itemBuilder: (context, pagePostion) {
+                    return widget.forms[pagePostion];
+                  }
+                )
+              ),
+            )
+          ],
+        )
       )
     );
   }
